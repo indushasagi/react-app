d58a46f9e6a2cb96b527a57c15626e328831045c
indushasagi <indusha.sagi@gmail.com>
2019-08-07 16:32:14 +0530

    added search feature

---
+++ src/components/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import Artist from "./Artist";
+
+const API_ADDRESS = "https://spotify-api-wrapper.appspot.com";
+
+class App extends Component {
+  state = { artistQuery: "", artist: null, tracks: [] };
+
+  updateArtistQuery = event => {
+    this.setState({ artistQuery: event.target.value });
+  };
+
+  searchArtist = () => {
+    fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+      .then(response => response.json())
+      .then(json => {
+        if (json.artists.total > 0) {
+          const artist = json.artists.items[0];
+          this.setState({ artist });
+
+          fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+            .then(response => response.json())
+            .then(json => this.setState({ tracks: json.tracks }))
+            .catch(error => alert(error.message));
+        }
+      })
+      .catch(error => alert(error.message));
+  };
+
+  handleKeyPress = event => {
+    if (event.key === "Enter") {
+      this.searchArtist();
+    }
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <h3>Music Master</h3>
+        <input
+          onChange={this.updateArtistQuery}
+          onKeyPress={this.handleKeyPress}
+          type="text"
+          placeholder="search"
+        />
+        <button onClick={this.searchArtist}>Search</button>
+        <Artist artist={this.state.artist} />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
-import React, { Component } from "react";
-import Title from "./components/Title";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 import "./styles.css";
 
-class App extends Component {
-  state = { displayBio: false };
-
-  toggleDisplayBio = () => {
-    this.setState({ displayBio: !this.state.displayBio });
-  };
-  render() {
-    return (
-      <div>
-        <h3>Hello!</h3>
-        <p>My name is Indu</p>
-        {this.state.displayBio ? <Title /> : null}
-
-        <button onClick={this.toggleDisplayBio}>Click</button>
-      </div>
-    );
-  }
-}
-
-export default App;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
